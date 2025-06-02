d9afb0392c6928e9f3d6d08b4ae75bdfb186e030
aulakh-karan <skaranpreet148@gmail.com>
2025-06-02 13:35:39 -0400

    Create system-info.js

---
+++ system-info.js
@@ -0,0 +1,9 @@
+const os = require("os");
+
+console.log("System Information:");
+console.log(`Operating System: ${os.type()}`);
+console.log(`Platform: ${os.platform()}`);
+console.log(`CPU Architecture: ${os.arch()}`);
+console.log(`Total Memory: ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`);
+console.log(`Free Memory: ${(os.freemem() / 1024 / 1024).toFixed(2)} MB`);
+console.log(`CPU Cores: ${os.cpus().length}`);
